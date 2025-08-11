6918030d078b0957ca0be894d746ffa9388feec3
Ignacio Gonzalez <igonpi13@gmail.com>
2025-08-11 11:59:37 -0300

    efe

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -33,7 +33,7 @@ type Resultado = {
   timestamp: string;
 };
 
-const API = 'https://croatia-pursue-abstracts-whale.trycloudflare.com ';
+const API = 'https://croatia-pursue-abstracts-whale.trycloudflare.com';
 const api = (path: string) => `${API}${path.startsWith('/') ? '' : '/'}${path}`;
 
 /** Grid de resultados (gridjs) */
